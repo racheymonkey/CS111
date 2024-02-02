df58f75213836c80210a872b48b98eb1ba3391fe
racheymonkey <131830335+racheymonkey@users.noreply.github.com>
2024-02-01 17:06:18 -0800

    Update pipe.c

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -1,65 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <sys/wait.h>
 #include <errno.h>
-#include <string.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s command1 command2 ... commandN\n", argv[0]);
+        // The lab specifies to exit with errno EINVAL if no commands are given.
         exit(EINVAL);
     }
 
     int pipes[argc - 1][2]; // One less than argc since argc includes the program name
     pid_t pid;
+    int status;
 
-    // Create pipes
+    // Create pipes for communication between processes
     for (int i = 0; i < argc - 2; i++) {
         if (pipe(pipes[i]) == -1) {
-            perror("pipe");
-            exit(errno);
+            exit(errno); // Exit with the errno set by pipe failure
         }
     }
 
     for (int i = 1; i < argc; i++) {
         pid = fork();
         if (pid == -1) {
-            perror("fork");
-            exit(errno);
+            exit(errno); // Exit with the errno set by fork failure
         }
 
         if (pid == 0) { // Child process
             if (i != 1) { // If not the first command, get input from previous pipe
-                if (dup2(pipes[i - 2][0], STDIN_FILENO) == -1) {
-                    perror("dup2");
-                    exit(errno);
-                }
+                dup2(pipes[i - 2][0], STDIN_FILENO);
+                close(pipes[i - 2][0]);
+                close(pipes[i - 2][1]);
             }
 
             if (i != argc - 1) { // If not the last command, output to next pipe
-                if (dup2(pipes[i - 1][1], STDOUT_FILENO) == -1) {
-                    perror("dup2");
-                    exit(errno);
-                }
-            }
-
-            // Close all pipe fds in the child process
-            for (int j = 0; j < argc - 2; j++) {
-                close(pipes[j][0]);
-                close(pipes[j][1]);
+                dup2(pipes[i - 1][1], STDOUT_FILENO);
+                close(pipes[i - 1][0]);
+                close(pipes[i - 1][1]);
             }
 
             execlp(argv[i], argv[i], (char *)NULL);
-            perror("execlp");
-            exit(errno);
+            exit(errno); // Exit with the errno set by execlp failure
         } else { // Parent process
             if (i != 1) {
-                close(pipes[i - 2][0]); // Close read end of the previous pipe
+                close(pipes[i - 2][0]);
             }
             if (i != argc - 1) {
-                close(pipes[i - 1][1]); // Close write end of the current pipe
+                close(pipes[i - 1][1]);
             }
         }
     }
