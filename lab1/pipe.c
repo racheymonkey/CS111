a1d9692209f99445b48a3a6ba8bbd1cf1a3b346c
racheymonkey <131830335+racheymonkey@users.noreply.github.com>
2024-02-01 18:31:11 -0800

    Update pipe.c

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -6,65 +6,77 @@
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        // The lab specifies to exit with errno EINVAL if no commands are given.
+        // exit with EINVAL if no commands are given
         exit(EINVAL);
     }
 
-    int pipes[argc - 1][2]; // One less than argc since argc includes the program name
+    // array of pipes to connect processes
+    int pipes[argc - 1][2]; // the number of pipes is one less than argc
+    
+    // store process IDs
     pid_t pid;
+    
+    // store child process status
     int status;
 
-    // Create pipes for communication between processes
+    // creating pipes for communication among processes
     for (int i = 0; i < argc - 2; i++) {
         if (pipe(pipes[i]) == -1) {
-            exit(errno); // Exit with the errno set by pipe failure
+            exit(errno);
         }
     }
 
     for (int i = 1; i < argc; i++) {
+        // creating a child process 
         pid = fork();
         if (pid == -1) {
-            exit(errno); // Exit with the errno set by fork failure
+            exit(errno);
         }
 
-        if (pid == 0) { // Child process
-            if (i != 1) { // If not the first command, get input from previous pipe
+        // child process
+        if (pid == 0) { 
+            if (i != 1) { // if not the first command, get input from previous command
                 dup2(pipes[i - 2][0], STDIN_FILENO);
+                // close read and write end of previous pipe
                 close(pipes[i - 2][0]);
                 close(pipes[i - 2][1]);
             }
 
-            if (i != argc - 1) { // If not the last command, output to next pipe
+            if (i != argc - 1) { // if not last command, output to next command
                 dup2(pipes[i - 1][1], STDOUT_FILENO);
                 close(pipes[i - 1][0]);
                 close(pipes[i - 1][1]);
             }
 
             execlp(argv[i], argv[i], (char *)NULL);
-            exit(errno); // Exit with the errno set by execlp failure
-        } else { // Parent process
+            exit(errno); 
+        } else { // parent process
             if (i != 1) {
+                // close read end of previous pipe
                 close(pipes[i - 2][0]);
             }
             if (i != argc - 1) {
+                // close write end of previous pipe
                 close(pipes[i - 1][1]);
             }
         }
     }
 
-    // Parent waits for all child processes to finish
+    // parent waits for all child processes to finish
     for (int i = 1; i < argc; i++) {
+        // wait for child processes to exit
         pid = waitpid(-1, &status, 0);
         if (pid == -1) {
-            exit(errno); // Exit with the errno set by waitpid failure
+            // if waitpid fails, exit
+            exit(errno);
         }
 
-        // If the child process exited with an error, reflect that in the parent's exit status.
+        // if child process exited with error, so does the parent
         if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
             exit(WEXITSTATUS(status));
         }
     }
 
-    // If all child processes exited successfully, return 0.
+    // if all child process exits properly, return 0
     return 0;
 }
